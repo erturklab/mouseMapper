430bcc54e9c95c1b2f931350c1a535e5b3a1ee59
Izabela <izabela.horvath@gmail.com>
2024-08-20 16:02:55 +0200

    added demo for tissue segmentation

--- tissue_segmentation/tissue_segmentation.py
+++ tissue_segmentation/tissue_segmentation.py
@@ -1,2 +1,34 @@
-def tissue_segmentation(config):
-    pass
+import nibabel as nib
+import numpy as np
+import os
+
+
+def readNifti(path,reorient=None):
+    '''
+    volume = readNifti(path)
+    
+    Reads in the NiftiObject saved under path and returns a Numpy volume.
+    This function can also read in .img files (ANALYZE format).
+    '''
+    if(path.find('.nii')==-1 and path.find('.img')==-1):
+        path = path + '.nii'
+    print(path)
+    if(os.path.isfile(path)):    
+        NiftiObject = nib.load(path)
+    elif(os.path.isfile(path + '.gz')):
+        NiftiObject = nib.load(path + '.gz')
+    else:
+        raise Exception("No file found at: "+path)
+    # Load volume and adjust orientation from (x,y,z) to (y,x,z)
+    volume = np.swapaxes(NiftiObject.dataobj,0,1)
+    if(reorient=='uCT_Rosenhain' and path.find('.img')):
+        # Only perform this when reading in raw .img files
+        # from the Rosenhain et al. (2018) dataset
+        #    y = from back to belly
+        #    x = from left to right
+        #    z = from toe to head
+        volume = np.swapaxes(volume,0,2) # swap y with z
+        volume = np.flip(volume,0) # head  should by at y=0
+        volume = np.flip(volume,2) # belly should by at x=0
+    return volume
+
